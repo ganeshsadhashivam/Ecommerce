28b770b6fb5969757bfb499ab6d6c60cbf36370f
ganesh <ganeshsadhashivam@outlook.com>
2023-04-08 23:03:20 +0530

    not done

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutPages = () => {
+  return <div>AboutPages</div>;
+};
+
+export default AboutPages;
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+import About from "./AboutPage";
+import Cart from "./Cart";
+import Error from "./Error";
+import Checkout from "./Checkout";
+import Private from "./PrivateRoute";
+//named export
+export { Home, Products, SingleProduct, About, Cart, Error, Checkout, Private };
